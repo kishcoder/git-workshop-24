0fcd3f9b465c8ba44611b5ca55a32019b0ae5f83
kishcoder <kpoojary2006@gmail.com>
2024-09-28 12:26:38 +0530

    add: kishcoder entry

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,6 +4,7 @@ export default function Navbar() {
   return (
     <div className="flex w-full fixed justify-between px-4 py-2 backdrop-blur-3xl border-b border-slate-800 z-50">
       <Image src={logo} alt="logo" width={200} height={100} />
+      HELLo
     </div>
   );
 }
